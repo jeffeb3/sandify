aeb2e4e7c6a957e138e86aa3250fb45585f5e0ea
James Donnelly <jamesjdonnelly@gmail.com>
2017-07-04 23:00:21 +0100

    Initial port of Turtle.js, with demo function included in initialisation.
    See: https://github.com/bjpop/js-turtle

--- src/App.js
+++ src/App.js
@@ -1,36 +1,97 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 import {
     Button,
-} from 'react-bootstrap'
-import './App.css';
+    ButtonGroup,
+    Col,
+    ControlLabel,
+    Form,
+    FormControl,
+    FormGroup,
+    ListGroup,
+    ListGroupItem,
+    Panel,
+} from 'react-bootstrap';
 
-import Header from './Header';
-import Documentation from './Documentation';
-import Shapes from './Shapes';
-import Transforms from './Transforms';
-import TurtleCanvas from './TurtleCanvas';
+import Turtle from './Turtle';
+//import TurtleCanvas from './TurtleCanvas';
 
 class App extends Component {
   render() {
+
+    function saySquare() {
+        alert('Square!')
+    }
+
+    function sayTriangle() {
+        alert('Triangle!')
+    }
 
     return (
       <div className="App">
 
         <div className="App-header">
-          <Header/>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>sandify</h2>
+          <p>
+            web based user interface to create patterns that
+            could be useful for robots that draw in sand with ball bearings.
+          </p>
         </div>
 
         <div className="App-left">
-          <Documentation/>
+          Sandify is working on a solution to turn your cold, empty hearted, emotionless sand tables into cold, empty hearted emotionless sand table robots with enchanting patterns.
+          <br/>
+          See the birthplace here:
+          <br/>
+          <a href="https://www.vicious1.com/forum/topic/does-this-count-as-a-build/">Vicious1.com Forum</a>
         </div>
 
         <div className="App-mid">
-          <Shapes />
-          <Transforms />
+
+          <ListGroup>
+            <ListGroupItem header="Square" active onClick={saySquare}>4 sided shape with equal sides and 90 degree angles</ListGroupItem>
+            <ListGroupItem header="Triangle" onClick={sayTriangle}>3 sided shape with equal sides and 120 degree angles</ListGroupItem>
+          </ListGroup>
+
+          <ButtonGroup>
+            <Button id="rotate" active>Spin</Button>
+            <Button id="scale">Grow</Button>
+          </ButtonGroup>
+
+          <Panel id="rotate-options" collapsible expanded={true}>
+            <Form horizontal>
+              <FormGroup controlId="rotate-step">
+                <Col componentClass={ControlLabel} sm={2}>
+                  Spin Speed
+                </Col>
+                <Col sm={10}>
+                  <FormControl type="number"/>
+                </Col>
+              </FormGroup>
+            </Form>
+          </Panel>
+
+          <Panel id="scale-options" collapsible expanded={true}>
+            <Form horizontal>
+              <FormGroup controlId="scale-step">
+                <Col componentClass={ControlLabel} sm={2}>
+                  Scale Speed
+                </Col>
+                <Col sm={10}>
+                  <FormControl type="number"/>
+                </Col>
+              </FormGroup>
+            </Form>
+          </Panel>
+
 
           <div>
-            <TurtleCanvas width={300} height={300} rotation={45}/>
+            <Turtle/>
           </div>
+
+
 
           <Button id="gcode" bsStyle="primary">Generate GCode</Button>
         </div>
