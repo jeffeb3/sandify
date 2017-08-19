461f7d34e7753c8abeb29793acfb79ccfd494359
Jeff Eberl <jeffeb3@gmail.com>
2017-08-19 16:04:28 -0600

    Added a wiper input.

--- src/reducers/Index.js
+++ src/reducers/Index.js
@@ -1,6 +1,10 @@
 
 import enforceLimits from '../LimitEnforcer';
-import { Vertex } from '../Geometry.js'
+import {
+  degToRad,
+  Vertex,
+} from '../Geometry.js'
+import Victor from 'victor';
 
 // Transform actions
 export const addShape = ( shape ) => {
@@ -60,6 +64,21 @@ export const toggleGrow = ( ) => {
 export const setGrow = ( value ) => {
   return {
     type: 'SET_GROW',
+    value: value,
+  };
+}
+
+// Wipe actions
+export const setWiperAngleDeg = ( value ) => {
+  return {
+    type: 'SET_WIPER_ANGLE_DEG',
+    value: value,
+  };
+}
+
+export const setWiperSize = ( value ) => {
+  return {
+    type: 'SET_WIPER_SIZE',
     value: value,
   };
 }
@@ -163,6 +182,9 @@ const defaultState = {
 
   turtleVertices: [],
 
+  wiperAngleDeg: 15,
+  wiperSize: 12,
+
   // Vertices
   vertices: [],
   input: 0,
@@ -259,10 +281,160 @@ const transformShapes = (state) => {
   return state;
 };
 
+const outOfBounds = (point, width, height) => {
+  if (point.x < -width/2.0) {
+    return true;
+  }
+  if (point.y < -height/2.0) {
+    return true;
+  }
+  if (point.x > width/2.0) {
+    return true;
+  }
+  if (point.y > height/2.0) {
+    return true;
+  }
+  return false;
+}
+
+// Intersect the line with the boundary, and return the point exactly on the boundary.
+// This will keep the shape. i.e. It will follow the line segment, and return the point on that line
+// segment.
+function boundPoint(good, bad, size_x, size_y) {
+  var dx = good.x - bad.x;
+  var dy = good.y - bad.y;
+
+  var fixed = Victor(bad.x, bad.y);
+  var distance = 0;
+  if (bad.x < -size_x || bad.x > size_x) {
+    if (bad.x < -size_x) {
+      // we are leaving the left
+      fixed.x = -size_x;
+    } else {
+      // we are leaving the right
+      fixed.x = size_x;
+    }
+    distance = (fixed.x - good.x) / dx;
+    fixed.y = good.y + distance * dy;
+    // We fixed x, but y might have the same problem, so we'll rerun this, with different points.
+    return boundPoint(good, fixed, size_x, size_y);
+  }
+  if (bad.y < -size_y || bad.y > size_y) {
+    if (bad.y < -size_y) {
+      // we are leaving the bottom
+      fixed.y = -size_y;
+    } else {
+      // we are leaving the top
+      fixed.y = size_y;
+    }
+    distance = (fixed.y - good.y) / dy;
+    fixed.x = good.x + distance * dx;
+  }
+  return fixed;
+}
+
+const wiper = (state) => {
+
+  var outputVertices = []
+
+  // Do the math
+
+  // Get the angle between 0,180
+  let angle = state.wiperAngleDeg % 180.0;
+  if (angle < 0.0) {
+    angle += 180.0;
+  }
+  console.log('angle:' + angle);
+  angle = degToRad(angle);
+
+  // Start with the defaults for 0,45
+  let height = state.max_y - state.min_y;
+  let width = state.max_x - state.min_x;
+  let startLocation = Victor(-width/2.0, height/2.0)
+  let orig_delta_w = Victor(state.wiperSize / Math.cos(angle), 0.0);
+  let orig_delta_h = Victor(0.0, -state.wiperSize / Math.sin(angle));
+
+  if (angle > Math.PI/4.0 && angle < 0.75 * Math.PI) {
+    // flip the logic of x,y
+    console.log('logic flipped angle:' + angle);
+    let temp = orig_delta_w.clone();
+    orig_delta_w = orig_delta_h.clone();
+    orig_delta_h = temp;
+  }
+  if (angle > Math.PI/2.0) {
+    console.log('flipping angle:' + angle);
+    startLocation = Victor(-width/2.0, -height/2.0)
+    orig_delta_w = orig_delta_w.clone().multiply(Victor(-1.0, -1.0));
+    orig_delta_h = orig_delta_h.clone().multiply(Victor(-1.0, -1.0));
+    console.log(orig_delta_h);
+  }
+  let delta_w = orig_delta_w;
+  let delta_h = orig_delta_h;
+  console.log('dw: ' + delta_w + ' dh: ' + delta_h);
+  outputVertices.push(startLocation);
+  let nextWidthPoint = startLocation;
+  let nextHeightPoint = startLocation;
+
+  let emergency_break = 0;
+  while (emergency_break < 1000) {
+    emergency_break += 1;
+
+    // "right"
+    nextWidthPoint = nextWidthPoint.clone().add(delta_w);
+    if (outOfBounds(nextWidthPoint, width, height)) {
+      let corner = boundPoint(nextWidthPoint.clone().subtract(delta_w), nextWidthPoint, width/2.0, height/2.0);
+      outputVertices.push(corner);
+      nextWidthPoint = boundPoint(nextHeightPoint, nextWidthPoint, width/2.0, height/2.0);
+      delta_w = orig_delta_h;
+    }
+    outputVertices.push(nextWidthPoint);
+
+    // "down-left"
+    nextHeightPoint = nextHeightPoint.clone().add(delta_h);
+    if (outOfBounds(nextHeightPoint, width, height)) {
+      nextHeightPoint = boundPoint(nextWidthPoint, nextHeightPoint, width/2.0, height/2.0);
+      delta_h = orig_delta_w;
+    }
+    outputVertices.push(nextHeightPoint);
+
+    // "down"
+    nextHeightPoint = nextHeightPoint.clone().add(delta_h);
+    outputVertices.push(nextHeightPoint);
+    if (outOfBounds(nextHeightPoint, width, height)) {
+      let corner = boundPoint(nextHeightPoint.clone().subtract(delta_h), nextHeightPoint, width/2.0, height/2.0);
+      outputVertices.push(corner);
+      nextHeightPoint = boundPoint(nextWidthPoint, nextHeightPoint, width/2.0, height/2.0);
+      delta_h = orig_delta_w;
+    }
+
+    outputVertices.push(nextHeightPoint);
+    if (outOfBounds(nextHeightPoint, width, height)) {
+      break;
+    }
+
+    // "up-right"
+    nextWidthPoint = nextWidthPoint.clone().add(delta_w);
+    outputVertices.push(nextWidthPoint);
+    if (outOfBounds(nextWidthPoint, width, height)) {
+      nextWidthPoint = boundPoint(nextHeightPoint, nextWidthPoint, width/2.0, height/2.0);
+      delta_w = orig_delta_h;
+    }
+
+    if (outOfBounds(nextWidthPoint, width, height)) {
+      break;
+    }
+  }
+
+  console.log(outputVertices);
+  setVerticesHelper(state, outputVertices);
+
+  return state;
+};
+
 const computeInput = (state) => {
   if (state.input === 0) {
     return transformShapes(state);
-  } else {
+  } else if (state.input === 1) {
     let newState = {
       ...state,
     }
@@ -270,6 +442,8 @@ const computeInput = (state) => {
     return Object.assign({}, state, {
       vertices: newState.vertices
     });
+  } else if (state.input === 2) {
+    return wiper(state);
   }
 }
 
@@ -349,6 +523,16 @@ const reducer  = (state = defaultState, action) => {
     case 'CHOOSE_INPUT':
       return computeInput({...state,
         input: action.value,
+      });
+
+    // Wiper Settings
+    case 'SET_WIPER_ANGLE_DEG':
+      return computeInput({...state,
+        wiperAngleDeg: action.value,
+      });
+    case 'SET_WIPER_SIZE':
+      return computeInput({...state,
+        wiperSize: action.value,
       });
 
     // Machine Settings
