1c9248d86bc3f819a37dd3555596eb45c3d0671c
Jeff Eberl <jeffeb3@gmail.com>
2019-01-13 21:56:43 -0700

    Added a heart shape.

--- src/inputs/Transforms.js
+++ src/inputs/Transforms.js
@@ -318,6 +318,24 @@ class ShapeList extends Component {
         ],
       });
     this.props.addShape({
+        name: "Heart",
+        vertices: (state) => {
+          let heart_points = []
+          for (let i=0; i<128; i++) {
+            let angle = Math.PI * 2.0 / 128.0 * i
+            // heart equation from: http://mathworld.wolfram.com/HeartCurve.html
+            heart_points.push(Vertex(1.0 * Math.pow(Math.sin(angle), 3),
+                                     13.0/16.0 * Math.cos(angle) +
+                                     -5.0/16.0 * Math.cos(2.0 * angle) +
+                                     -2.0/16.0 * Math.cos(3.0 * angle) +
+                                     -1.0/16.0 * Math.cos(4.0 * angle)))
+          }
+          return heart_points
+        },
+        options: [
+        ],
+      });
+    this.props.addShape({
         name: "Text",
         vertices: (state) => {
           let points = [];
