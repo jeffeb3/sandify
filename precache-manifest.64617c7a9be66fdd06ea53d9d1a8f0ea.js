aa55b43b321c3e2b9211dd4324ca9cfddb0e55bc
Jeff Eberl <jeffe@NeyaSystems.com>
2019-02-23 16:28:44 -0700

    Updates

---
+++ precache-manifest.64617c7a9be66fdd06ea53d9d1a8f0ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6ab37e624aecf6887178c5293ddae75",
+    "url": "/sandify/static/media/logo.a6ab37e6.svg"
+  },
+  {
+    "revision": "1d0255d1bd1dcfaa10f7",
+    "url": "/sandify/static/js/runtime~main.1d0255d1.js"
+  },
+  {
+    "revision": "5e45f421e8d53ebaf0de",
+    "url": "/sandify/static/js/main.5e45f421.chunk.js"
+  },
+  {
+    "revision": "bbb5a1450503c7c76cbb",
+    "url": "/sandify/static/js/2.bbb5a145.chunk.js"
+  },
+  {
+    "revision": "5e45f421e8d53ebaf0de",
+    "url": "/sandify/static/css/main.e91ab130.chunk.css"
+  },
+  {
+    "revision": "bbb5a1450503c7c76cbb",
+    "url": "/sandify/static/css/2.1ba76a7a.chunk.css"
+  },
+  {
+    "revision": "08a0859448ad9111a647190721acfbae",
+    "url": "/sandify/index.html"
+  }
+];
