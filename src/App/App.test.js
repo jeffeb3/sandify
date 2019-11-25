74f0b6d30350f2e9a2bc1ca188136b4e22c8f1b4
Jeff Eberl <jeffeb3@gmail.com>
2019-11-24 22:01:58 -0700

    Revert "Working on limits some, including adding some tests."
    This reverts commit a7208d1be0aed1bd394dd2040ed5051c265713c7.

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('tests work', () => {
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
-
-// it actually doesn't
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-// });
