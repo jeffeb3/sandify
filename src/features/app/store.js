3ed3b0d9442de3eb647ba5c94bdedd59d45fea36
Bob Carmichael <bobc@edfinity.com>
2020-04-04 06:31:08 -0400

    don't save shape state in local storage

--- src/features/app/store.js
+++ src/features/app/store.js
@@ -7,20 +7,12 @@ import gcodeReducer from '../gcode/gCodeSlice'
 import shapesReducer from '../shapes/shapesSlice'
 import transformsReducer from '../transforms/transformsSlice'
 import turtleReducer from '../turtle/turtleSlice'
+import { registeredShapes } from '../../common/registeredShapes'
 import {
-  loadState,
-  saveState
- } from '../../common/localStorage'
- import { registeredShapes } from '../../common/registeredShapes'
- import {
-   addShape,
-   setCurrentShape,
-   updateShape
- } from '../shapes/shapesSlice'
- import {
-   addTransform,
-   updateTransform
-  } from '../transforms/transformsSlice'
+  addShape,
+  setCurrentShape
+} from '../shapes/shapesSlice'
+import { addTransform } from '../transforms/transformsSlice'
 
 const store = configureStore({
   reducer: combineReducers({
@@ -45,34 +37,8 @@ Object.keys(registeredShapes).forEach(key => {
   store.dispatch(addShape(state))
 })
 
-// override default values with saved ones
-const persistedState = loadState()
-
-if (persistedState) {
-  Object.keys(persistedState.shapes.byId).forEach((key) => {
-    let shape = persistedState.shapes.byId[key]
-    shape.id = key
-    store.dispatch(updateShape(shape))
-  })
-
-  Object.keys(persistedState.transforms.byId).forEach((key) => {
-    let transform = persistedState.transforms.byId[key]
-    transform.id = key
-    store.dispatch(updateTransform(transform))
-  })
-}
-
 const storedShape = localStorage.getItem('currentShape')
 const currentShape = storedShape && registeredShapes[storedShape] ? storedShape : 'polygon'
-
 store.dispatch(setCurrentShape(currentShape))
-store.subscribe(() => {
-  const state = store.getState()
-
-  saveState({
-    shapes: state.shapes,
-    transforms: state.transforms
-  })
-})
 
 export default store
