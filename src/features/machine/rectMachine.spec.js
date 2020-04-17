36a96a857dfa2ac60e6a2b9da6af7022c79ed51c
Jeff Eberl <jeffeb3@gmail.com>
2020-04-16 21:47:00 -0600

    Test the onPerimeter function.

--- src/features/machine/rectMachine.spec.js
+++ src/features/machine/rectMachine.spec.js
@@ -7,6 +7,45 @@ describe('rect machine', () => {
     machine = new RectMachine([], {minX: 0, maxX: 500, minY: 0, maxY: 500})
   })
 
+  describe('onPerimeter', () => {
+    it('when points on minX, returns true', () => {
+      let p1 = {x: -250, y: 0}
+      let p2 = {x: -250, y: 50}
+      expect(machine.onPerimeter(p1, p2)).toEqual(true)
+    })
+
+    it('when points on different X, returns false', () => {
+      let p1 = {x: 250, y: 0}
+      let p2 = {x: -250, y: 50}
+      expect(machine.onPerimeter(p1, p2)).toEqual(false)
+    })
+
+    it('when points on different X (same Y), returns false', () => {
+      let p1 = {x: 250, y: 50}
+      let p2 = {x: -250, y: 50}
+      expect(machine.onPerimeter(p1, p2)).toEqual(false)
+    })
+
+    it('when points on minY, returns true', () => {
+      let p1 = {x: 0,  y: -250}
+      let p2 = {x: 50, y: -250}
+      expect(machine.onPerimeter(p1, p2)).toEqual(true)
+    })
+
+    it('when points on different Y, returns false', () => {
+      let p1 = {x: 0,  y: 250}
+      let p2 = {x: 50, y: -250}
+      expect(machine.onPerimeter(p1, p2)).toEqual(false)
+    })
+
+    it('when points on different Y (same X), returns false', () => {
+      let p1 = {x: 50, y: -250}
+      let p2 = {x: 50, y: 250}
+      expect(machine.onPerimeter(p1, p2)).toEqual(false)
+    })
+
+  })
+
   describe('tracePerimeter', () => {
     it('when points on the same line, no connecting points needed', () => {
       let p1 = {x: 50, y: -250}
