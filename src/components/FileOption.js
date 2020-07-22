9be2f9db7fef8a16add426817b0aadcba13faad0
texx00 <texx91@gmail.com>
2020-07-22 22:20:22 +0200

    The image is now saved into a canvas and loaded from there to create the points. Also set correct initial trasform options

--- src/components/FileOption.js
+++ src/components/FileOption.js
@@ -21,27 +21,31 @@ class ImageOption extends Component {
     const model = this.props.model
     const optionType = option.type || 'file'
     const visible = option.isVisible === undefined ? true : option.isVisible(model)
+    const canvasId = option.canvasId || 'preview-image'
+    const canvasVisible = option.canvasVisible === undefined ? true : option.canvasVisible
 
     return (<div>
       <Row className={"align-items-center pb-1 " + (visible ? null : ' d-none')}>
         <Col sm={5}>
-        <Form.Label htmlFor="options-step">
+        <Form.Label htmlFor="options-image">
           {option.title}
         </Form.Label>
         </Col>
 
         <Col sm={7}>
           <Form.Control
-            id="options-step"
+            id="options-image"
             type={optionType}
             accept=".png"
             onChange={(event) => {
               let attrs = {}
-              let value = event.target.files[0]   // get the loaded file
-              let fr = new FileReader()
-              let im = new Image()
-              let canvas = document.getElementById("preview-canvas")  // get the preview element
+              let file = event.target.files[0]    // get the loaded file
+              let fr = new FileReader()           // prepare the file reader to load the image to an url
+              let im = new Image()                // image object to write to canvas
+              let canvas = document.getElementById(canvasId
+          )  // get the canvas element
               let context = this                  // save it for the callback
+              let value
               
               fr.onload = function(){             // filereader callback
                 im.onload = function(){           // image loaded callback
@@ -49,6 +53,7 @@ class ImageOption extends Component {
                   canvas.height = 150
                   let scale = Math.min(canvas.width/im.width, canvas.height/im.height)
                   ctx.drawImage(im, 0, 0, im.width*scale, im.height*scale)
+                  value = true
                   
                   // need to set the state from the callback otherwise the dataurl will be wrong
                   attrs[context.props.optionKey] = value
@@ -60,19 +65,19 @@ class ImageOption extends Component {
                     context.delayedSet(value, context.props.delayKey, context.props.onChange)
                   }
                 }
-                value = fr.result       // need to save the value as a serialized object
-                im.src = value          // use the value for the preview
+                im.src = fr.result    // set the image from url
               };
-              fr.readAsDataURL(value)
+              fr.readAsDataURL(file)
             }}
             />
         </Col>
       </Row>
-      <Row className={"align-items-center pb-1 " + (visible ? null : ' d-none')}>
+      <Row className={"align-items-center pb-1 " + (canvasVisible && visible ? null : ' d-none')}>
           <Col sm={5}>
           </Col>
           <Col sm={7}> 
-            <canvas id="preview-canvas" height="0"></canvas>
+            <canvas id={canvasId
+      } height="0"></canvas>
           </Col>
       </Row>
       </div>
--- src/models/BWImage.js
+++ src/models/BWImage.js
@@ -1,12 +1,16 @@
 import Victor from 'victor'
 import Shape, { shapeOptions } from './Shape'
 
+const canvasId = 'bwimage_canvas'
+
 const options = {
   ...shapeOptions,
   ...{
     image_file: {
       title: 'Load image',
-      type: 'file'
+      type: 'file',
+      canvasId: canvasId,
+      canvasVisible: false
     }
   }
 }
@@ -21,49 +25,53 @@ export default class BWImage extends Shape {
       ...super.getInitialState(),
       ...{
         type: 'bwimage',
-        image_file: ""
+        image_file: false,
       }
     }
   }
 
+  getInitialTransformState() {
+    return {
+      startingSize: 10,
+      canChangeSize: true,
+      numLoops: 1,
+      transformMethod: "intact"
+    }
+  }
+
   getVertices(state) {
-    let value = state.shape.image_file
+    let file_loaded = state.shape.image_file
     let points = []
 
-    if (value !== ""){
+    if (file_loaded){
       try{
-        let im = new Image()
-        let canvas = document.createElement("canvas")
-        im.onload = function(){
-            let ctx = canvas.getContext('2d')
-            ctx.drawImage(im, 0, 0)
-            let w = canvas.width
-            let h = canvas.height
-            let step = 10
-            let x_left_side = true
-            points.push(new Victor(0,0))
-            // must normalize the points scale to be between 0 and 1
-            for(let i=0; i<h;  i+=step){
-              if(x_left_side){
-                points.push(new Victor(0,i/h))   // should use -Infinity/+Infinity but the library cannot handle it. Must check the preview how is done and set a boundary for the points there
-              }else{
-                points.push(new Victor(1,i/h))
-              }
-              x_left_side = !x_left_side
-              for(let j=0; j<w; j++){
-                
-              }
-            }
+        // need to save the file into a canvas and load from it
+        // cannot save any value of the image into the state (because it is not serializable and it will slow down everything too much)
+        let canvas = document.getElementById(canvasId)
+        let ctx = canvas.getContext('2d')
+        let image = ctx.getImageData(0, 0, canvas.width, canvas.height)
+        let w = image.width
+        let h = image.height
+        let step = 10
+        let x_left_side = true
+        points.push(new Victor(0,0))
+        // must normalize the points scale to be between 0 and 1
+        for(let i=0; i<h;  i+=step){
+          if(x_left_side){
+            points.push(new Victor(0,i/h))   // should use -Infinity/+Infinity but the library cannot handle it. Must check the preview how is done and set a boundary for the points there
+          }else{
+            points.push(new Victor(1,i/h))
+          }
+          x_left_side = !x_left_side
+          for(let j=0; j<w; j++){
+            
+          }
         }
-        im.src = value
-        //document.body.appendChild(canvas);
       }
       catch(err){
         console.log(err)
       }
     }
-    // need to add a way to wait for the points to be ready
-    //TODO remove repetitions (or loop)
     return points
   }
 
