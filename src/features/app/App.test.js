30b02ebbfeaa9d58960cdd21f0c1da1498f2b57b
Bob Carmichael <bobc@edfinity.com>
2020-12-27 17:56:09 -0500

    a number of minor package updates

--- src/features/app/App.test.js
+++ src/features/app/App.test.js
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import 'core-js'
+import Konva from 'konva-node'
+
+Konva.isBrowser = false
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
