1eccc0304807c92d3dfc583fd9d49f1bac22e434
Bob Carmichael <bobc@edfinity.com>
2021-01-01 16:49:48 -0500

    never persist state when running specs

--- src/features/app/store.js
+++ src/features/app/store.js
@@ -37,7 +37,8 @@ const persistState = false
 const persistInitKey = 'state'
 const persistSaveKey = 'state'
 
-if (persistState) {
+// never persist state when running within a jest spec
+if (process.env.JEST_WORKER_ID === undefined && persistState) {
   // override default values with saved ones
   const persistedState = loadState(persistInitKey)
 
