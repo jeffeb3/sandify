0eacd24279f2a48729de54ceb65f90714b5aa3f6
Guillaume Besson <guillaume@besson.co>
2022-04-10 17:11:29 +0200

    Fix exporterSlice tests

--- src/features/exporter/exporterSlice.spec.js
+++ src/features/exporter/exporterSlice.spec.js
@@ -1,29 +1,25 @@
-import exporter, {
-  updateExporter
-} from './exporterSlice'
+import exporter, { updateExporter } from "./exporterSlice";
 
-describe('exporter reducer', () => {
-  it('should handle initial state', () => {
+describe("exporter reducer", () => {
+  it("should handle initial state", () => {
     expect(exporter(undefined, {})).toEqual({
-      fileName: 'sandify',
-      fileType: 'gcode',
+      fileName: "sandify",
+      fileType: "gcode",
       polarRhoMax: 1,
       unitsPerCircle: 6,
-      pre: '',
-      post: '',
+      pre: "",
+      post: "",
+      pngPreview: false,
       reverse: false,
-      show: false
-    })
-  })
+      show: false,
+    });
+  });
 
-  it('should handle updateExporter', () => {
+  it("should handle updateExporter", () => {
     expect(
-      exporter(
-        {filename: ''},
-        updateExporter({filename: 'test_filename'})
-      )
+      exporter({ filename: "" }, updateExporter({ filename: "test_filename" }))
     ).toEqual({
-      filename: 'test_filename'
-    })
-  })
-})
+      filename: "test_filename",
+    });
+  });
+});
