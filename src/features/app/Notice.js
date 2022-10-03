aa000752fe859dcc2ff06fd5b369b351c05b58dc
Jeff Eberl <jeffeb3@gmail.com>
2022-10-03 15:45:30 -0600

    Point to the algorithmic entry page instead.

--- src/features/app/Notice.js
+++ src/features/app/Notice.js
@@ -7,7 +7,7 @@ class Notice extends Component {
     return (
       <div className="notice d-flex align-items-center justify-content-center">
         <p className="ml-4 d-none d-lg-block">
-            <a href="https://sisyphus.wishpondpages.com/track-design-contest-freestyle-entry/">Use Sandify to enter the Sisyphus design competition! Enter by Oct 16th.</a>
+            <a href="https://sisyphus.wishpondpages.com/track-design-contest-algorithmic-entry/">Use Sandify to enter the Sisyphus design competition! Enter by Oct 16th.</a>
         </p>
       </div>
     )
