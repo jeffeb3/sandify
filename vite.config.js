c09b47b387a5afb77b0a85f750f62141223623f2
Bob Carmichael <bobc@edfinity.com>
2022-11-06 08:36:09 -0500

    polyfill the node functions needed by gcode-toolpath

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,33 @@
 import fs from 'fs/promises';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import nodePolyfills from 'rollup-plugin-node-polyfills'
 
 export default defineConfig(() => ({
   plugins: [react()],
   resolve: {
     alias: {
-      // necessary to load gcode-toolpath
-      'stream': 'stream-browserify',
+      process: "rollup-plugin-node-polyfills/polyfills/process-es6",
+      timers: "rollup-plugin-node-polyfills/polyfills/timers",
+      stream: "rollup-plugin-node-polyfills/polyfills/stream",
+      events: "rollup-plugin-node-polyfills/polyfills/events",
+      util: "rollup-plugin-node-polyfills/polyfills/util",
+      buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6"
     }
   },
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    target: 'esnext',
+    rollupOptions: {
+      plugins: [nodePolyfills()],
+    }
   },
-  // We use this configuration to treat .js file as .jsx. In the future we should rename them all to .jsx instead and remove this config
+  define: {
+    "process.env": process.env ?? {},
+  },
+  // We use this configuration to treat .js file as .jsx. In the future we should rename them all
+  // to .jsx instead and remove this config
   esbuild: {
     loader: "jsx",
     include: /src\/.*\.jsx?$/,
@@ -22,6 +36,7 @@ export default defineConfig(() => ({
   optimizeDeps: {
     esbuildOptions: {
       plugins: [
+        NodeGlobalsPolyfillPlugin({ buffer: true }),
         {
           name: "load-js-files-as-jsx",
           setup(build) {
